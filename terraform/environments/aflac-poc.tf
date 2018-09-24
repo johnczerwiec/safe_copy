fdb20426f14878f17e9a8ac750388730bd727ea8
INT\makapa <manan.kapadia@ensono.com>
2018-09-24 09:47:02 -0500

    fixed error

--- terraform/environments/aflac-poc.tf
+++ terraform/environments/aflac-poc.tf
@@ -6,7 +6,7 @@ module environment {
   cidr               = "10.42.36.0/25"                      # e.g. 10.72.192.0.21
   region             = "us-east-2"                          # e.g. eu-west-1
   ci_prefix          = "afl40"                              # e.g. ccm43
-  ci_suffix          = "poc                                 # e.g. ccm43
+  ci_suffix          = "poc"                                # e.g. ccm43
   monitoring_tag     = "not-required"                       # e.g. datadog|not-required
   backup_tag         = "no-backup"                          # e.g. ccm_poc_daily - make sure to use _ not -
   key_name           = "aflac-poc"                          # e.g. ccm-poc 
