be7cfdd302137fb353a6a20c4cba2e980b694289
INT\makapa <manan.kapadia@ensono.com>
2018-09-24 09:45:54 -0500

    updated file for ec2 deployment

--- terraform/blueprints/aflac-poc/instances.tf
+++ terraform/blueprints/aflac-poc/instances.tf
@@ -6,15 +6,15 @@
    ami                   	 = "ami-0d2cde2c50d4d1fc2"
   instance_type          	 = "m5.4xlarge"
   key_name               	 = "${var.key_name}"
-  vpc_security_group_ids	 = [ "${aws_security_group.ensono_mgt.id}" ]
+  vpc_security_group_ids	 = [ "${aws_security_group.ensono_mgmt.id}" ]
   subnet_id              	 = "${aws_subnet.privateAzA.id}"
   iam_instance_profile  	 = "${module.iam_role_WEB.iam_instance_profile}"
   ebs_optimized         	 = "${var.ebs_opt_web}"
   user_data 			 	 = "${data.template_file.windows_userdata.rendered}"
-#  disable_api_termination 	 = "true"
+# disable_api_termination 	 = "true"
   tags {
-    Name                 = "${var.ci_prefix}web01${var.prod_ci_suffix}"
-    Description          = "${var.prod_ci_suffix} Web Instance"
+    Name                 = "${var.ci_prefix}gic01${var.ci_suffix}"
+    Description          = "${var.ci_suffix} App Instance"
     }
  root_block_device {
     volume_type = "gp2"
--- terraform/blueprints/aflac-poc/variables.tf
+++ terraform/blueprints/aflac-poc/variables.tf
@@ -7,6 +7,7 @@ variable "vpc_name"       { default=""   description="e.g. Prod VPC" }
 variable "cidr"           { default=""   description="e.g. 10.72.176.0/21" }
 variable "region"         { default=""   description="e.g. eu-west-1" }
 variable "ci_prefix"      { default=""   description="e.g. ccm43" }
+variable "ci_suffix"      { default=""   description="e.g. ccm43" }
 variable "monitoring_tag" { default=""   description="e.g. datadog|not-required" }
 variable "backup_tag"     { default=""   description="e.g. ccm_poc_daily" }
 variable "s3_bucket"      { default=""   description="e.g. ccm-poc" }
