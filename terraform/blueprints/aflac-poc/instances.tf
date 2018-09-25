87ad869d4a52f1884e618f93fa9af758ad613c60
INT\makapa <manan.kapadia@ensono.com>
2018-09-25 08:46:56 -0500

    fixed the subent name

--- terraform/blueprints/aflac-poc/instances.tf
+++ terraform/blueprints/aflac-poc/instances.tf
@@ -2,12 +2,12 @@
 # afl40gic01poc
 #-----------------------------------------------------
   resource "aws_instance" "afl40gic01poc" {
-#  lifecycle { prevent_destroy = "true" }
-   ami                   	 = "ami-0d2cde2c50d4d1fc2"
+# lifecycle { prevent_destroy = "true" }
+  ami                   	 = "ami-0d2cde2c50d4d1fc2"
   instance_type          	 = "m5.4xlarge"
   key_name               	 = "${var.key_name}"
   vpc_security_group_ids	 = [ "${aws_security_group.ensono_mgmt.id}" ]
-  subnet_id              	 = "${aws_subnet.privateAzA.id}"
+  subnet_id              	 = "${aws_subnet.PrivateSbA.id}"
   iam_instance_profile  	 = "${module.iam_role_WEB.iam_instance_profile}"
   ebs_optimized         	 = "${var.ebs_opt_web}"
   user_data 			 	 = "${data.template_file.windows_userdata.rendered}"
