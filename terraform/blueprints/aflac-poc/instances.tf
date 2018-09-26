51be94b328f635e59485ef5ee26e518b9f32690a
INT\makapa <manan.kapadia@ensono.com>
2018-09-26 09:52:37 -0500

    removed the space

--- terraform/blueprints/aflac-poc/instances.tf
+++ terraform/blueprints/aflac-poc/instances.tf
@@ -1,7 +1,7 @@
 #----------------------------------------------------- 
 # afl40gic01poc
 #-----------------------------------------------------
-  resource "aws_instance" "afl40gic01poc" {
+resource "aws_instance" "afl40gic01poc" {
 # lifecycle { prevent_destroy = "true" }
   ami                   	 = "ami-0d2cde2c50d4d1fc2"
   instance_type          	 = "m5.4xlarge"
@@ -12,6 +12,7 @@
   ebs_optimized         	 = "${var.ebs_opt_web}"
   user_data 			 	 = "${data.template_file.windows_userdata.rendered}"
 # disable_api_termination 	 = "true"
+
   tags {
     Name                 = "${var.ci_prefix}gic01${var.ci_suffix}"
     Description          = "${var.ci_suffix} App Instance"
