c9d8914d5123b63fce4a8602ae66ad58419cb162
INT\makapa <manan.kapadia@ensono.com>
2018-09-28 11:50:46 -0500

    added peering info

---
+++ terraform/blueprints/aflac-poc/vpc_peering.tf
@@ -0,0 +1,11 @@
+
+#-------------------------------------------
+# VPC Peering Connection
+#-------------------------------------------
+
+data "aws_vpc_peering_connection" "aflac-poc-adminvpc-peering" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  peer_cidr_block = "10.96.0.0/24"
+  peer_vpc_id = "vpc-85e11ce3"
+ 
+}
