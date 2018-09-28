9667e12c1ab0b5d1d1adbc9a2a32a79a43179765
INT\makapa <manan.kapadia@ensono.com>
2018-09-28 12:15:09 -0500

    fixing error

--- terraform/blueprints/aflac-poc/vpc_peering.tf
+++ terraform/blueprints/aflac-poc/vpc_peering.tf
@@ -6,6 +6,6 @@
 data "aws_vpc_peering_connection" "aflac-poc-adminvpc-peering" {
   vpc_id = "${aws_vpc.vpc.id}"
   peer_cidr_block = "10.96.0.0/24"
-  peer_vpc_id = "vpc-85e11ce3"
+  peer_vpc_id = "vpc-0c8451b45b6036607"
  
 }
