9cb25182a33e7b512ed4e0434aa976666711c5a2
INT\sramamoorthy <srinivasan.ramamoorthy@ensono.com>
2018-10-04 15:03:26 -0500

    adding the vpc peering file

--- terraform/blueprints/aflac-poc/vpc_peering.tf
+++ terraform/blueprints/aflac-poc/vpc_peering.tf
@@ -3,7 +3,7 @@
 # VPC Peering Connection
 #-------------------------------------------
 
-data "aws_vpc_peering_connection" "aflac-poc-adminvpc-peering" {
+data "aws_vpc_peering_connection" "aflacpoc-adminvpc-peering" {
   vpc_id = "${aws_vpc.vpc.id}"
   peer_cidr_block = "10.96.0.0/24"
   peer_vpc_id = "vpc-0c8451b45b6036607"
