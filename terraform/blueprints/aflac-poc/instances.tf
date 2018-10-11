ebecbfa3dd1f9f47685df34d1aefa2d77cf75ad5
INT\makapa <manan.kapadia@ensono.com>
2018-10-11 08:14:15 -0500

    added instanc 2

--- terraform/blueprints/aflac-poc/instances.tf
+++ terraform/blueprints/aflac-poc/instances.tf
@@ -27,3 +27,33 @@ resource "aws_instance" "gic01" {
     volume_size = "50"
   }
 }
+
+#----------------------------------------------------- 
+# afl40gic02poc
+#-----------------------------------------------------
+resource "aws_instance" "gic02" {
+# lifecycle { prevent_destroy = "true" }
+  ami                   	 = "ami-0d546cb42ded83d49"
+  instance_type          	 = "m5.4xlarge"
+  key_name               	 = "${var.key_name}"
+  vpc_security_group_ids	 = [ "${aws_security_group.ensono_mgmt.id}", "${aws_security_group.cust_sg.id}" ]
+  subnet_id              	 = "${aws_subnet.PrivateSbA.id}"
+  iam_instance_profile       = "${module.iam_role_Web.iam_instance_profile}"
+  ebs_optimized         	 = "${var.ebs_opt_web}"
+  user_data 			 	 = "${template_file.windows_userdata.rendered}"
+# disable_api_termination 	 = "true"
+
+  tags {
+    Name                 = "${var.ci_prefix}gic02${var.ci_suffix}"
+    Description          = "${var.ci_suffix} App Instance"
+    }
+ root_block_device {
+    volume_type = "gp2"
+    volume_size = "500"
+  }
+ ebs_block_device {
+    device_name = "xvdb"
+    volume_type = "gp2"
+    volume_size = "50"
+  }
+}
