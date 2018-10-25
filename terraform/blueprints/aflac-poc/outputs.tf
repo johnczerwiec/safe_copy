8159c60daa7c84240625854ea6450b37c34f0dfe
INT\makapa <manan.kapadia@ensono.com>
2018-10-25 15:05:56 -0500

    fixed error

--- terraform/blueprints/aflac-poc/outputs.tf
+++ terraform/blueprints/aflac-poc/outputs.tf
@@ -1,3 +1,2 @@
-output "private_subnetA"         		{ value = "${aws_subnet.privateAzA.id}"}
-output "private_subnetB"         		{ value = "${aws_subnet.privateAzB.id}"}
-output "private_subnetC"         		{ value = "${aws_subnet.privateAzC.id}"}
+output "private_subnetA"         		{ value = "${aws_subnet.PrivateSbA.id}"}
+output "private_subnetB"         		{ value = "${aws_subnet.PrivateSbB.id}"}
