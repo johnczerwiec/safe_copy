2e8e58418209664223a79e93cdcadd09a490f22d
INT\makapa <manan.kapadia@ensono.com>
2018-10-25 15:11:12 -0500

    fixed erorr

--- terraform/environments/aflac-poc/outputs.tf
+++ terraform/environments/aflac-poc/outputs.tf
@@ -1,2 +1,2 @@
-output "private_subnetA"         		{ value = "${module.environment.PrivateSbA"}
-output "private_subnetB"         		{ value = "${module.environment.PrivateSbB"}
+output "private_subnetA"         		{ value = "${module.environment.PrivateSbA}"}
+output "private_subnetB"         		{ value = "${module.environment.PrivateSbB}"}
