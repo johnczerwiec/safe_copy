238e40dc71f6b096a2acc1ac1aba436ebf50fa09
INT\makapa <manan.kapadia@ensono.com>
2018-10-25 15:04:52 -0500

    added outputl file

---
+++ terraform/blueprints/aflac-poc/outputs.tf
@@ -0,0 +1,3 @@
+output "private_subnetA"         		{ value = "${aws_subnet.privateAzA.id}"}
+output "private_subnetB"         		{ value = "${aws_subnet.privateAzB.id}"}
+output "private_subnetC"         		{ value = "${aws_subnet.privateAzC.id}"}
