66477b6c28fce964f8178b8e5453145e3cc99530
INT\makapa <manan.kapadia@ensono.com>
2018-10-25 14:57:32 -0500

    added output file

---
+++ terraform/environments/aflac-poc/outputs.tf
@@ -0,0 +1,3 @@
+output "private_subnetA"         		{ value = "${module.environment.private_subnetA}"}
+output "private_subnetB"         		{ value = "${module.environment.private_subnetB}"}
+#output "private_subnetC"         		{ value = "${module.environment.private_subnetC}"}
