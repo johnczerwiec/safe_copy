4d179f77ff501aac9e67c99155bff7d43d662e0a
INT\sramamoorthy <srinivasan.ramamoorthy@ensono.com>
2018-12-04 14:22:51 -0600

    updated the ingress rule

--- terraform/blueprints/aflac-poc/security_groups.tf
+++ terraform/blueprints/aflac-poc/security_groups.tf
@@ -53,6 +53,13 @@ resource "aws_security_group" "cust_sg" {
     protocol = "tcp"
     cidr_blocks = ["10.0.0.0/8", "10.48.24.0/24", "172.16.0.0/12", "192.168.0.0/16" ]
   }
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "10.48.24.0/24" ]
+  }  
  
    ingress {
     from_port = 27000
